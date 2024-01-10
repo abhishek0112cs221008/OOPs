838766a617b81418a400fba0336bdb2c9a047846
Abhishek Patel <149716322+abhishek0112cs221008@users.noreply.github.com>
2024-01-10 21:15:00 +0530

    File Handling in c++

---
+++ File handling/write_on_file.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+//writing on file
+
+int main()
+{
+    ofstream fout;
+    fout.open("ABHISHEK.txt");
+
+    fout<<"Hello Abhishek Patel !!"<<endl;
+    fout<<"Hi Abhishek Patel !!"<<endl;
+
+    fout.close();
+
+    return 0;
+}
+
+/*
+   ios::in                [input/read] 
+   ios::out               [output/write] 
+   ios::app               [append] 
+   ios::ate               [update] 
+   ios::binaru            [binary] 
+*/
