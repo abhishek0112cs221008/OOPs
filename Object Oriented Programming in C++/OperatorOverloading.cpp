ded8151eef056fbfd95fa6cb7962799781b43b9b
Abhishek Patel <149716322+abhishek0112cs221008@users.noreply.github.com>
2024-10-08 16:39:46 +0530

    Update the class and Constructor OperatorOverloading.cpp

--- Object Oriented Programming in C++/OperatorOverloading.cpp
+++ Object Oriented Programming in C++/OperatorOverloading.cpp
@@ -6,9 +6,14 @@ class Complex
 private:
     int real,imag;
 public:
-    Complex(){
-        real = 4;
-        imag = 9;
+    Complex() {
+        real = 0;
+        imag = 0;
+    }
+    
+    Complex(int real, int imag){
+        this->real = real;
+        this->imag = imag;
     }
 
     void show(){
@@ -24,11 +29,11 @@ public:
 };
 
 int main(){
-    Complex c1;
+    Complex c1(2,3);
     c1.show();
 
     
-    Complex c2;
+    Complex c2(4,5);
     c2.show();
 
     Complex c3;
@@ -37,3 +42,40 @@ int main(){
 
     return 0;
 }
+
+// class Complex
+// {
+// private:
+//     int real,imag;
+// public:
+//     Complex(){
+//         real = 4;
+//         imag = 9;
+//     }
+
+//     void show(){
+//         cout<<real<<" + "<<imag<<"i"<<endl;
+//     }
+
+//     Complex operator+(Complex &c){
+//         Complex t;
+//         t.real = real + c.real;
+//         t.imag = imag + c.imag;
+//         return t;
+//     }
+// };
+
+// int main(){
+//     Complex c1;
+//     c1.show();
+
+    
+//     Complex c2;
+//     c2.show();
+
+//     Complex c3;
+//     c3 = c1 + c2;
+//     c3.show();
+
+//     return 0;
+// }
